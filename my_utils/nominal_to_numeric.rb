411939576112dfa932bb20f315555b05fdc77647
Phillip Birmingham <Phillip.Birmingham@fidelitylife.com>
2016-03-06 21:35:46 -0600

    modify sampling script to remove fnlwght column

--- my_utils/nominal_to_numeric.rb
+++ my_utils/nominal_to_numeric.rb
@@ -11,22 +11,24 @@ TRAINING_FRACTION = 0.7
 count = 0
 field_names = []
 field_values = []
-numeric_fields = [0, 2, 4, 10, 11, 12] #these are supposed to be numbers
+fields_to_skip = [2]
+numeric_fields = [0, 4, 10, 11, 12] #these are supposed to be numbers
 
 training_out = File.new 'adult_training.csv', 'w'
 testing_out = File.new 'adult_testing.csv', 'w'
 
 
-CSV.foreach '../src/opt/test/Adult.csv' do |row|
-  if count == 0 #get field names from first row
-    row.each do |field|
-      field_names << field
+INPUT_FILENAME = '../src/opt/test/Adult-small.csv'
+
+CSV.foreach INPUT_FILENAME do |row|
+
+  row_out_data = []
+  row.each_index do |i|
+    if count == 0 #get field names from first row
+      field_names << row[i]
       field_values << {}
-    end
-  else
-    row_out_data = []
-    row.each_index do |i|
-      if numeric_fields.include? i #don't translate columns that are numeric -- repore any non-numerics i might not have seen
+    else
+      if numeric_fields.include? i #don't translate columns that are numeric -- report any non-numerics i might not have seen
         if row[i].is_number?
           row_out_data << row[i]
         else
@@ -34,12 +36,13 @@ CSV.foreach '../src/opt/test/Adult.csv' do |row|
           raise StandardError.new "Non-numeric value in numeric column #{i} line #{count + 1}"
         end
       else
-        if !field_values[i][row[i]]  #we don't have a translation for this value, add one
-          field_values[i][row[i]] = field_values[i].length
-        end
-        row_out_data << field_values[i][row[i]]  #replace with the translated value
+        field_values[i][row[i]] = field_values[i].length if !field_values[i][row[i]]
+        row_out_data << field_values[i][row[i]] if !fields_to_skip.include? i #replace with the translated value
       end
+
     end
+  end
+  if count > 0
     if Random.rand(1.0) < TRAINING_FRACTION
       training_out << row_out_data.join(',') + "\n"
     else
@@ -53,9 +56,11 @@ testing_out.close
 
 fout = File.new 'key.txt', 'w'
 field_names.each_index do |i|
-  fout.print "Field: #{field_names[i]}\n"
-  field_values[i].each { |name, id| fout.print "#{name}: #{id}\n" }
-  fout.print "----------\n"
+  if !fields_to_skip.include? i
+    fout.print "Field: #{field_names[i]}\n"
+    field_values[i].each { |name, id| fout.print "#{name}: #{id}\n" }
+    fout.print "----------\n"
+  end
 
 end
 
