6c27849bb0c110e8942d26939def62204b6ba087
Phillip J. Birmingham <phillip@pjbsoftware.com>
2016-03-20 21:11:28 -0500

    Initial files -- Randomized runner needs some work

---
+++ src/shared/test/IndepenentComponentAnalysisRunner.java
@@ -0,0 +1,34 @@
+package shared.test;
+
+import shared.DataSet;
+import shared.Instance;
+import shared.filt.IndependentComponentAnalysis;
+import shared.reader.ArffDataSetReader;
+import util.linalg.Matrix;
+import util.linalg.RectangularMatrix;
+
+/**
+ * A class for testing
+ * @author Andrew Guillory gtg008g@mail.gatech.edu
+ * @version 1.0
+ */
+public class IndepenentComponentAnalysisRunner {
+    
+    /**
+     * The test main
+     * @param args ignored
+     */
+    public static void main(String[] args) throws Exception {
+        String filename = args[0];
+        ArffDataSetReader reader = new ArffDataSetReader(filename);
+        DataSet set = reader.read();
+        System.out.println("Before ICA");
+        System.out.println(set);
+        IndependentComponentAnalysis filter = new IndependentComponentAnalysis(set, 1);
+        filter.filter(set);
+        System.out.println("After ICA");
+        System.out.println(set);
+          
+    }
+
+}
---
+++ src/shared/test/LinearDiscriminantAnalysisRunner.java
@@ -0,0 +1,37 @@
+package shared.test;
+
+import shared.DataSet;
+import shared.Instance;
+import shared.filt.LinearDiscriminantAnalysis;
+import shared.reader.ArffDataSetReader;
+import util.linalg.DenseVector;
+
+/**
+ * A class for testing
+ * @author Andrew Guillory gtg008g@mail.gatech.edu
+ * @version 1.0
+ */
+public class LinearDiscriminantAnalysisRunner {
+    
+    /**
+     * The test main
+     * @param args ignored
+     */
+    public static void main(String[] args) throws Exception {
+        String filename = args[0];
+        ArffDataSetReader reader = new ArffDataSetReader(filename);
+        DataSet set = reader.read();
+        System.out.println("Before LDA");
+        System.out.println(set);
+        LinearDiscriminantAnalysis filter = new LinearDiscriminantAnalysis(set);
+        filter.filter(set);
+        System.out.println(filter.getProjection());
+        System.out.println("After LDA");
+        System.out.println(set);
+        filter.reverse(set);
+        System.out.println("After reconstructing");
+        System.out.println(set);
+        
+    }
+
+}
