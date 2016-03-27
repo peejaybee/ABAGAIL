9a75b626d1d1134c2ccc285d950b344a6ae1c499
Phillip J. Birmingham <phillip@pjbsoftware.com>
2016-03-27 16:11:33 -0500

    fix command to run perceptron

--- do-titanic.sh
+++ do-titanic.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
- weka.classifiers.functions.MultilayerPerceptron -L 0.3 -M 0.2 -N 500 -V 0 -S 0 -E 20 -H a
+java -cp weka.jar weka.classifiers.functions.MultilayerPerceptron -L 0.15 -M 0.2 -N 500 -V 0 -S 0 -E 20 -H a -t titanic3-normalized-testing.arff -k
